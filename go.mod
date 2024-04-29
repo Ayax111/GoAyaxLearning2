3587c4ffc0a8aa3bc7d09ca30eb490013f14f36a
Ayax111 <ayaxacme@gmail.com>
2024-04-29 18:16:43 +0200

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ayax111/GoAyaxLearning2
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Ayax111/GoAyaxLearning2/variables"
+)
+
+func main() {
+	fmt.Println("Hola Mundo!!!")
+	variables.MuestroEnteros()
+
+}
